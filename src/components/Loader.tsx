d9e4a31e6bcb891db25e40566e6260e332ca8e25
Oğulcan Çevik <ogulcan@assatekno.com>
2023-05-02 13:34:52 +0300

    loader added.

---
+++ src/components/Loader.tsx
@@ -0,0 +1,12 @@
+const Loader = () => {
+  return (
+    <div class="lds-ring">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Loader;
