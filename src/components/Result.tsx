13304c8f4e5d4a4fe3e04bc1ee735308ff2b9d60
Oğulcan Çevik <ogulcan@assatekno.com>
2023-05-02 13:35:09 +0300

    result section added.

---
+++ src/components/Result.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'preact/hooks';
+
+const Result = ({ choises, resetGame }: { choises: boolean[], resetGame: () => void }) => {
+  const [correctCount, setCorrectCount] = useState<number>(0);
+  const [wrongCount, setWrongCount] = useState<number>(0);
+  useEffect(() => {
+    choises.forEach((choise) => {
+      if (choise) {
+        setCorrectCount((prev) => prev + 1);
+      } else {
+        setWrongCount((prev) => prev + 1);
+      }
+    });
+  }, []);
+  return (
+    <div class="flex flex-col justify-center gap-10 items-center w-screen h-screen text-[#33cfffff] text-2xl font-mono min-w-[320px] text-center max-w-[900px]">
+      <h1>Result</h1>
+      <div class="flex flex-col gap-5">
+        <p>Correct: {correctCount}</p>
+        <p>Wrong: {wrongCount}</p>
+      </div>
+      <div>
+        <button onClick={resetGame} class="bg-[#33cfffff] text-black px-5 py-2 rounded-md hover:bg-[#33cfffff] hover:text-white transition-all">
+          Play Again
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Result;
